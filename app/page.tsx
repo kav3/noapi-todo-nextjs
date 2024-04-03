2d271cbc1ed736f9c317fc83d68d4cb333c6a206
Kaveh <kav3.com@gmail.com>
2024-04-03 18:48:22 +0330

    chore: push to deply

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import { FaGithub, FaCheck, FaTrash, FaPlus } from "react-icons/fa";
 
 const api = new Client()
 
-
 export default function Home() {
   const [todos, setTodods] = useState<types.Todo[]>()
   const [title, setTitle] = useState<string>()
@@ -39,9 +38,7 @@ export default function Home() {
   }, [])
 
   return (<span className="flex flex-col gap-2 justify-center items-center bg-gray-900 min-h-full">
-
     <h1 className="flex text-2xl text-white font-bold">noapi-todo-nextjs</h1>
-
     <span className="max-w-full p-8 bg-gray-800 rounded-lg shadow-lg w-96 text-gray-200 items-center">
       {todos?.map((todo) => (
         <span>
@@ -52,11 +49,9 @@ export default function Home() {
             </span>
             <span className="flex flex-1 ml-4 text-sm">{todo.title}</span>
             <button onClick={e => api.collection("todo").delete(todo._id)} className='text-slate-700 hover:text-white'><FaTrash /></button>
-
           </label>
         </span>
       ))}
-
       <form onSubmit={submit} className="flex items-center w-full h-8 px-2 mt-2 text-sm font-medium rounded">
         <button type="submit">
           <FaPlus size={22} />
@@ -64,7 +59,6 @@ export default function Home() {
 
         <input onChange={e => setTitle(e.target.value)} value={title} className="flex-grow h-8 ml-4 bg-transparent focus:outline-none font-medium" type="text" placeholder="add a new task" />
       </form>
-
     </span>
     <span className="flex">
       <a target='_blank' className='flex gap-1 items-center text-sm text-white' href="https://github.com/kav3/noapi-todo-nextjs-example"><FaGithub size={16} /> github </a>
